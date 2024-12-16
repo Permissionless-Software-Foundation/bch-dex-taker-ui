faa8e54806c30c5664dbe35989ce8008c071d1c0
Chris Troutner <chris.troutner@gmail.com>
2024-12-16 05:19:18 -0800

    Adding polyfills for nostr libraries

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    vm: require.resolve('vm-browserify'),
+  };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ]);
+
+  // console.log(JSON.stringify(config, null, 2));
+
+  return config;
+};
