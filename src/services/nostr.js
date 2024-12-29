4e8477bafeda931cf06888bd6ff9b638ab9c9365
Chris Troutner <chris.troutner@gmail.com>
2024-12-29 13:34:17 -0800

    Using nostr-tools for publish to a Relay

--- src/services/nostr.js
+++ src/services/nostr.js
@@ -5,7 +5,7 @@
 
 // Global npm libraries
 import { finalizeEvent } from '@chris.troutner/nostr-tools/pure'
-import { Relay } from '@chris.troutner/nostr-tools/relay'
+import { Relay } from 'nostr-tools/relay'
 import BchNostr from 'bch-nostr'
 import * as nip19 from '@chris.troutner/nostr-tools/nip19'
 
